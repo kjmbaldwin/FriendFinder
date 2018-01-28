7281596aeaa31a8d48971d27d9d418c52b6ff67b
Kyle Baldwin <kylejmbaldwin@gmail.com>
2018-01-28 18:33:33 -0500

    initial file structure and server.js setup

---
+++ server.js
@@ -0,0 +1,21 @@
+//dependencies 
+var express = require("express");
+var bodyParser = require("body-parser"); 
+var path = require('path');
+
+//express server setup
+var app = express();
+var PORT = process.env.PORT || 8080;  //heroku port or local 8000
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+require("./app/data/friends")(app);
+
+//start server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
