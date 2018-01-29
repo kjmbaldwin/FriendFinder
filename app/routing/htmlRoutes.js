db6addb73f9bac0374c0ae99bd4de8a03c33a966
Kyle Baldwin <kylejmbaldwin@gmail.com>
2018-01-28 20:32:17 -0500

    created home.html, and 2 html page routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,9 @@
+//home route
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+//survey route
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
