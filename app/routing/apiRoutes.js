3c1df4c3f5f578199edc5fb8f1e316140dcacd75
Kyle Baldwin <kylejmbaldwin@gmail.com>
2018-01-30 23:36:31 -0500

    math is working to compare each value to the submitted value

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,23 @@ module.exports = function(app) {
     });
 
     app.post("/api/new", function(req, res){
+        
+        for (let i = 0; i < friendsArray.length; i++) {
+            
+            var currentFriend = friendsArray[i];
+            var totalDifference = 0;
+
+            for (let j = 0; j < currentFriend.scores.length; j++) {
+
+                var friendScore = parseInt(currentFriend.scores[j]); 
+                var myScore = parseInt(req.body.scores[j])
+
+                totalDifference += Math.abs(friendScore - myScore);
+
+                console.log("total differnece: " + totalDifference);
+            }
+        }
+        
         friendsArray.push(req.body);
         console.log(req.body);
     });
